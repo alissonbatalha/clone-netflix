c5b6a5040d01c46e1aaf08c6e9286a2bca0a6099
alisson batalha <alissonlsbatalhaa@gmail.com>
2022-11-06 10:02:35 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,12 @@
+/*const pergunta = document.querySelector(".pergunta");
+const resposta = document.querySelector(".resposta");
+	
+
+function mostraResposta (elemento) {
+		elemento.classList.toggle('resposta--ativa');
+}
+
+
+pergunta.addEventListener("click", () =>  {
+	console.log('fui clicado');
+});*/
